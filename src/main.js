a98ec31557d2112d025b6b37f0e8aaf1d631763f
tuber <btu@thoughtworks.com>
2016-11-08 23:55:26 +0800

    [tuber] use babel plugin to process lodash.

--- src/main.js
+++ src/main.js
@@ -1,7 +1,9 @@
 'use strict';
 import {numA} from './a';
-import reduce from 'lodash-es/reduce';
+import _ from 'lodash';
 //import {reduce, filter} from 'lodash-es';
-console.log(reduce([1, 2, 3], (pre, nxt)=> pre + nxt, 0));
+console.log(_.reduce([1, 2, 3], (pre, nxt)=> pre + nxt, 0));
+console.log(_.filter([1, 2, 3], item=> item > 1));
+console.log(_.map([1, 2, 3], item=> item));
 //console.log(filter([1, 2, 3], item=> item==1));
 console.log(numA());
